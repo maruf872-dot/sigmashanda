54498a70429d67bcc4629bdde99026eda467d65b
maruf872-dot <maruf872@gmail.com>
2026-04-25 13:24:25 +0600

    Add files via upload

---
+++ pythonNew2.py
@@ -0,0 +1,12 @@
+#Printing a new sentence
+print("Welcome to Python Programming")
+
+#Printing a integer
+print(4)
+
+#Printing a new line after print
+print("Hello python \n")
+
+#Printing all the keywords present in python
+import keyword
+print(keyword.kwlist)
