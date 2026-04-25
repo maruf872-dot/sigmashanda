632f186736c93d589be4059cf7f11a35702c5f17
maruf872-dot <maruf872@gmail.com>
2026-04-25 13:17:30 +0600

    Add files via upload

---
+++ pythonNew.py
@@ -0,0 +1,12 @@
+# Printing a greeting message
+print("Hello, World!")
+
+#Printing my name
+
+print("My name is jawadullah")
+
+#Printing a fun fact about myself
+print("I love to play football")
+
+#Performing and printing a simple calculation
+print("The sum of 3 and 5 is:", 3 + 5)
