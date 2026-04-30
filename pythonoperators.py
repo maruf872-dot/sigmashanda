09871f569666658ecba5cfa1877f2f1b27f0aceb
maruf872-dot <maruf872@gmail.com>
2026-04-30 17:02:48 +0600

    Add files via upload

---
+++ pythonoperators.py
@@ -0,0 +1,21 @@
+num1 = 24
+num2 = 12
+
+print("Number 1 :", num1)
+print("Number 2 :", num2)
+print("Addition :"  , num1 + num2)
+print("Subtraction :"  , num1 - num2)
+print("Multiplication :"  , num1 * num2)
+print("Division :"  , num1 / num2)
+print("Floor Division :"  , num1 // num2)
+print("Modulus Operation :"  , num1 % num2)
+print("Square :", num2 ** 2)
+print("Square Root :", num1 ** 0.5)
+
+print("Equal ?", num1==num2)
+print("Number 1 greater?", num1 > num2)
+print("Number 2 greater?", num2 > num1)
+print("Not Equal ?", num1 != num2)
+
+result = num1/2+num2**2+10
+print("Result of give equation is ", result)
