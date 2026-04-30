ca2c7b0c081e952acd0c550acd05fb970f3aa7ce
maruf872-dot <maruf872@gmail.com>
2026-04-30 16:51:15 +0600

    Add files via upload

---
+++ DataTypes.py
@@ -0,0 +1,22 @@
+name = "Abdullah bin Abdulaziz Al-Saud"
+age = 12
+is_student = True
+weight = 51.5
+
+print("My name is ", name)
+print("Data type of name is ",  type(name))
+print("My age is ",  age)
+print("Data type of age is ",  type(age))
+print("I am a student ",  is_student)
+print("Data type of is_student is ",  type(is_student))
+print("My weight is ",  weight)
+print("Data type of weight is ",  type(weight))
+
+print("\n After Type Casting....")
+
+age=str(age)
+print(age)
+print("Data type of age is ",  type(age))
+weight = int(weight)
+print(weight)
+print("Data type of weight is ",  type(weight))
