917cf15d5b613ea23f723b18a30d07639ebdfdbb
maruf872-dot <maruf872@gmail.com>
2026-05-02 13:33:50 +0600

    Add files via upload

---
+++ nestedconditions.py
@@ -0,0 +1,11 @@
+num = int(input("Enter a number : "))
+
+if num>50:
+    print("The number is greater than 50")
+    if num%2==0:
+        print("And it is even too")
+    else:
+        print("And it is odd too")
+else:
+    print("The number is less than or equal to 50")
+       
