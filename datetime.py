95ebd9ddee3b28c6b8d605201af5b4705ff302af
maruf872-dot <maruf872@gmail.com>
2026-05-02 13:42:49 +0600

    Add files via upload

---
+++ datetime.py
@@ -0,0 +1,10 @@
+import datetime
+
+current_time = datetime.datetime.now()
+
+print("The time now at Dhaka cantonment, Dhaka is : ")
+print(current_time)
+
+import calendar
+print("\n",calendar.calendar(2026))
+
